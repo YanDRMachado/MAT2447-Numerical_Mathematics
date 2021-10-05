92383961f9417930d39b5fc116543853aacb217f
Yan D. R. Machado <80362698+YanDRMachado@users.noreply.github.com>
2021-10-04 22:05:39 -0300

    Update Numerical Integration - Midpoint, Trapez, Simpson.py

--- Numerical Integration - Midpoint, Trapez, Simpson.py
+++ Numerical Integration - Midpoint, Trapez, Simpson.py
@@ -4,43 +4,43 @@
 
 import math
 
-f = lambda x: x**2 *math.cos(x) 
+# f = lambda x: x**2 *math.cos(x) 
 
-a = 0
-b = math.pi/2
-N = 5 #número de subintervalos
-h = (b-a)/N #"step size"
-S = 1/2 * (f(a) + f(b))
+# a = 0
+# b = math.pi/2
+# N = 5 #número de subintervalos
+# h = (b-a)/N #"step size"
+# S = 1/2 * (f(a) + f(b))
 
-for i in range(1, n): #no python isso vai de 1 até n-1
-    S += f(a + i*h) #i é o subscrito do nosso x [x1, x2, x3, ...]
+# for i in range(1, n): #no python isso vai de 1 até n-1
+#     S += f(a + i*h) #i é o subscrito do nosso x [x1, x2, x3, ...]
 
-Integral = h*S
-print("Integral = %f" %Integral)
+# Integral = h*S
+# print("Integral = %f" %Integral)
 
 
-'''implementando numpy, matplotlib e calculando um trapézio singular para aproximar a integral'''
+# '''implementando numpy, matplotlib e calculando um trapézio singular para aproximar a integral'''
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-#plot da função, do trapézio singular que apeoxima a área da integral e cálculo dessa área
-x = np.linspace(-0.5, 1.5, 100)
-y = np.exp(-x**2)
-plt.plot(x,y)
+# #plot da função, do trapézio singular que apeoxima a área da integral e cálculo dessa área
+# x = np.linspace(-0.5, 1.5, 100)
+# y = np.exp(-x**2)
+# plt.plot(x,y)
 
-x0 = 0
-x1 = 1
-y0 = np.exp(-x0**2) 
-y1 = np.exp(-x1**2)
-plt.fill_between([x0,x1],[y0,y1])
+# x0 = 0
+# x1 = 1
+# y0 = np.exp(-x0**2) 
+# y1 = np.exp(-x1**2)
+# plt.fill_between([x0,x1],[y0,y1])
 
-plt.xlim([-0.5, 1.5])
-plt.ylim([0, 1.5])
-plt.show()
+# plt.xlim([-0.5, 1.5])
+# plt.ylim([0, 1.5])
+# plt.show()
 
-Integral = 0.5*(y1 + y0)*(x1 - x0)
-print("Integral = %f" % Integral)
+# Integral = 0.5*(y1 + y0)*(x1 - x0)
+# print("Integral = %f" % Integral)
 
 '''definindo a fórmula trapezoidal como uma função, versão final + erro + plots'''
 
